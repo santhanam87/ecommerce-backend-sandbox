d07258023790f5cc6e1c4e331b41d3949531e851
Santhanam Elumalai <sansandy04@gmail.com>
2025-12-12 21:30:25 -0700

    adding guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,21 +6,22 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/udpate-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseDto } from './dto/user-response.dto';
-import { PasswordUtil } from 'src/common/utils/password.util';
-import { SignInUserDto } from './dto/singin-user.dto';
-
+import { AuthGuard } from '@nestjs/passport';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   public getAllUsers(): UserResponseDto[] {
     return this.usersService.getAllUsers();
   }
+  @UseGuards(AuthGuard('jwt'))
   @Get(':userName')
   public getUserById(@Param('userName') id: string) {
     const user = this.usersService.getUserById(id);
@@ -33,16 +34,7 @@ export class UsersController {
   public createUser(@Body() body: CreateUserDto) {
     return this.usersService.createUser(body);
   }
-  @Post('/signin')
-  public signInUser(@Body() body: SignInUserDto) {
-    // Sign in logic to be implemented
-    const { userName, password } = body;
-    const user = this.usersService.getUserById(userName);
-    if (user && PasswordUtil.comparePasswords(password, user.password)) {
-      return new UserResponseDto(user);
-    }
-    return { message: 'Invalid credentials' };
-  }
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':userName')
   public updateUser(
     @Param('userName') userName: string,
@@ -50,6 +42,7 @@ export class UsersController {
   ) {
     return this.usersService.updateUser(userName, body);
   }
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   public deleteUserById(@Param('id') id: string) {
     return this.usersService.deleteUserById(id);
