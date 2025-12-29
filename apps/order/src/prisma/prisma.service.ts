069b9fdebd55785b6a80619257f0f57abd4b936b
Santhanam Elumalai <sansandy04@gmail.com>
2025-12-28 21:31:39 -0700

    order entity

---
+++ apps/order/src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    // Load the connection string from environment variables
+    const connectionString = process.env.DATABASE_URL as string;
+    // Instantiate the PostgreSQL adapter
+    const adapter = new PrismaPg({ connectionString });
+
+    // Pass the adapter to the PrismaClient constructor
+    super({ adapter });
+  }
+}
