be5161fb7602c0e56c2320df208dfa4e4e26764a
Santhanam Elumalai <sansandy04@gmail.com>
2026-03-22 18:12:17 -0600

    pim integration

---
+++ apps/product-information-service/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { resolve } from 'node:path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        resolve(process.cwd(), '.env'),
+        resolve(__dirname, '..', '.env'),
+      ],
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    AppService,
+  ],
+})
+export class AppModule {}
